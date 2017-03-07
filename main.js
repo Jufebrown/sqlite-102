b50e2939ee52dbd4db2785dea52f533ee9a7efa7
Jufebrown <jufebrown@gmail.com>
2017-03-07 14:35:36 -0600

    added function to populate employee table

--- main.js
+++ main.js
@@ -10,12 +10,14 @@ const dropEmployees = () => {
   db.run(`DROP TABLE employees`)
 }
 
+// dropEmployees()
+
 // Create a table titled employees with the following columns:
 // id, firstName, lastName, jobTitle, address
 db.run(`CREATE TABLE IF NOT EXISTS employees (id INT PRIMARY KEY, firstName TEXT, lastName TEXT, jobTitle TEXT, address TEXT)`)
 
 // Create an array of at least 6 objects. Each object should have a key value pair matching each column name in the employees table.
-let array = [
+let employeeArray = [
   {id: 0, firstName: 'Fred', lastName: 'Flintstone', jobTitle: 'Crane Operator', address: '1 Bedrock Way'},
   {id: 0, firstName: 'Wilma', lastName: 'Flintstone', jobTitle: 'Homemaker', address: '1 Bedrock Way'},
   {id: 0, firstName: 'Chloe', lastName: 'Catherine', jobTitle: 'Lounger', address: '513 Stacy Square'},
@@ -24,8 +26,18 @@ let array = [
   {id: 0, firstName: 'Matt', lastName: 'Smith', jobTitle: 'Doctor', address: '1 Milky Way'},
 ]
 
-
 // Insert each of the employee objects into the database.
+const populateEmployees = () => {
+  employeeArray.forEach(each => {
+    db.run(`INSERT INTO employees VALUES (
+      null,
+      "${each.firstName}",
+      "${each.lastName}",
+      ${each.jobTitle},
+      "${each.address}"
+    )`)
+  })
+}
 
 // Write a statement to query the database and console.log() all employee records.
 
